ff4a5e26cabab59b123041653f97c1a1d4edd6d7
Robert Wójtowicz <rkwojtowicz@gmail.com>
2022-01-06 14:08:36 +0100

    2.3

--- import_mqtt.sh
+++ import_mqtt.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Data acquisition from MQTT broker
 user=admin
 passwd=password
@@ -7,7 +9,7 @@ echo 'Mi Body Composition Scale 2 Garmin Connect v2.3'
 echo ''
 
 # Create a data backup file
-path=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+path=`cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd`
 if [ ! -f $path/backup.csv ] ; then
 	echo '* Create a data backup file, checking for new data'
 	echo 'Weight;Impedance;Units;User;Unix_time;Readable_time;Bat_in_V;Bat_in_%' > $path/backup.csv
