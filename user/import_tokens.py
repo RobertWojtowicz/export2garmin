2f40ea64a3a56d5f9c64ce789a6f8b1e32078fa1
Robert Wójtowicz <rkwojtowicz@gmail.com>
2024-07-08 15:25:09 +0200

    1.x

--- user/import_tokens.py
+++ user/import_tokens.py
@@ -30,7 +30,7 @@ def init_api():
 # Create Oauth1 and Oauth2 tokens as base64 encoded string
         tokenstore_base64 = os.path.dirname(os.path.dirname(__file__))
         token_base64 = garmin.garth.dumps()
-        dir_path = os.path.expanduser(tokenstore_base64 + '/user/' + email)
+        dir_path = os.path.expanduser(tokenstore_base64 + email)
         with open(dir_path, "w") as token_file:
             token_file.write(token_base64)
         print(datetime.datetime.now().strftime("%d.%m.%Y-%H:%M:%S") + " * Oauth tokens saved correctly")
