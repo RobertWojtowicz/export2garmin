ed1a3d8cb1177d928521e3cea7c61fa3e33307e9
Robert Wójtowicz <rkwojtowicz@gmail.com>
2026-05-28 15:04:40 +0200

    3.7

--- user/import_tokens.py
+++ user/import_tokens.py
@@ -42,8 +42,7 @@ def init_api():
         garmin = Garmin(email, password, is_cn=tokens_is_cn, prompt_mfa=get_mfa)
         garmin.login(token_file)
         print(f"{ts()} * OAuth tokens saved correctly: {token_file}")
-    except (
-        FileNotFoundError, GarminConnectAuthenticationError, GarminConnectConnectionError, GarminConnectTooManyRequestsError) as err:
+    except (FileNotFoundError, GarminConnectAuthenticationError, GarminConnectConnectionError, GarminConnectTooManyRequestsError) as err:
         print(f"{ts()} * ERROR: {err}")
         return None
 
